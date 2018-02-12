633ea2c259d06823e7e4d37fc127fd28cfb21113
David Cabeza <cabezadavide@gmail.com>
2018-02-12 18:45:43 -0400

    Adds toBinary method

---
+++ main.hs
@@ -0,0 +1,16 @@
+-- Universidad Simón Bolívar
+-- CI3661: Programming Languages I
+--
+-- First Project Haskell
+--
+-- Authors:
+--
+-- David Cabeza <13-10191@usb.ve>
+-- Fabiola Martínez <13-10838@usb.ve>
+--
+-- February, 2018.
+
+-- converts an integer to its binary representation
+toBinary :: Int-> [Int]
+toBinary 0 = []
+toBinary x = toBinary (x `div` 2) ++ [x `rem` 2]
